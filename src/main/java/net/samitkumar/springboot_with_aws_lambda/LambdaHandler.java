6268be5c1a8130212699ca52450c9c15b75ccbb6
samitkumarpatel <samitkumarpatel@gmail.com>
2026-03-30 13:17:22 +0000

    added support for HttpApiV2Proxy

--- src/main/java/net/samitkumar/springboot_with_aws_lambda/LambdaHandler.java
+++ src/main/java/net/samitkumar/springboot_with_aws_lambda/LambdaHandler.java
@@ -1,8 +1,10 @@
 package net.samitkumar.springboot_with_aws_lambda;
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.internal.servlet.AwsHttpApiV2ProxyHttpServletRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
@@ -12,12 +14,13 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class LambdaHandler implements RequestStreamHandler {
-    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    private static final SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handlerV2;
     static {
         try {
-            SpringBootLambdaContainerHandler.getContainerConfig()
-                    .setInitializationTimeout(30_000);
+            SpringBootLambdaContainerHandler.getContainerConfig().setInitializationTimeout(30_000);
             handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(SpringbootWithAwsLambdaApplication.class);
+            handlerV2 = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(SpringbootWithAwsLambdaApplication.class);
         } catch (ContainerInitializationException e) {
             // if we fail here. We re-throw the exception to force another cold start
             e.printStackTrace();
